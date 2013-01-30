08dd6ef088c0798200020a07ec8df277f95ec807
Sebastiano Peluso <peluso@gsd.inesc-id.pt>
2013-01-30 11:15:33 +0100

    import deleted

--- test/eu/cloudtm/wpm/connector/TestConnector.java
+++ test/eu/cloudtm/wpm/connector/TestConnector.java
@@ -4,10 +4,7 @@ import java.net.UnknownHostException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
-import eu.cloudtm.wpm.logService.remote.events.SubscribeEvent;
-import eu.cloudtm.wpm.logService.remote.listeners.TestWPMStatisticsRemoteListenerImpl;
 import eu.cloudtm.wpm.logService.remote.listeners.TestWPMViewChangeRemoteListenerImpl;
-import eu.cloudtm.wpm.logService.remote.listeners.WPMStatisticsRemoteListener;
 import eu.cloudtm.wpm.logService.remote.listeners.WPMViewChangeRemoteListener;
 
 public class TestConnector {
